7570106792c8238a35061e912c4e9cd8d12df6d6
Román Martínez <rgmf@riseup.net>
2025-01-13 19:26:43 +0100

    Bugfixes: a simple one.

--- src/main/java/com/proferoman/App.java
+++ src/main/java/com/proferoman/App.java
@@ -3,7 +3,7 @@ package com.proferoman;
 public class App {
     public static void main(String[] args) {
         System.out.println("Estos son los argumentos que has pasado:");
-        for (int i = 1; i <= args.length; i++) {
+        for (int i = 0; i < args.length; i++) {
             System.out.println(args[i]);
         }
     }
