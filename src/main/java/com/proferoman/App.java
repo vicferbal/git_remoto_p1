f840a88c528ce1cd0b546eafa0fba66ac40f23ff
Román Martínez <rgmf@riseup.net>
2025-01-14 10:29:43 +0100

    Program improvements.

--- src/main/java/com/proferoman/App.java
+++ src/main/java/com/proferoman/App.java
@@ -2,9 +2,11 @@ package com.proferoman;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Estos son los argumentos que has pasado:");
+        System.out.println("Argumentos pasados al programa:");
         for (int i = 1; i <= args.length; i++) {
-            System.out.println(args[i]);
+            System.out.println("Argumento " + i + ": " + args[i]);
         }
+
+        System.out.println("Fin del programa.");
     }
 }
