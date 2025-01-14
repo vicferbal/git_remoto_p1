3eca68aecd27cbad88047cb26896b73e3fad9799
Román Martínez <rgmf@riseup.net>
2025-01-14 10:33:17 +0100

    Several improvements.

--- src/main/java/com/proferoman/App.java
+++ src/main/java/com/proferoman/App.java
@@ -2,9 +2,11 @@ package com.proferoman;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Estos son los argumentos que has pasado:");
-        for (int i = 1; i <= args.length; i++) {
-            System.out.println(args[i]);
+        System.out.println("Listado de argumentos pasados al programa:");
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("Argumento: " + (i + 1) + ": " + args[i]);
         }
+
+        System.out.println("Fin del programa. Hasta pronto.");
     }
 }
